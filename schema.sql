cc82ead9fc9ffe06f964303d1fa1038feb62186d
irizon <irizon.m@gmail.com>
2020-05-18 17:53:19 +0900

    userオンリーにしました

--- schema.sql
+++ schema.sql
@@ -2,8 +2,5 @@ CREATE table user(
      id int not null primary key,
      name text not null,
      pass_hash text not null,
-     teamname text not null,
-     datanum int not null primary key,
-     day text not null,
-     player text not null,
+     teamname text not null
 )
