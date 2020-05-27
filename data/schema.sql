b5620657124165627b3f5517e86d42294e79755e
eggplants <w10776e8w@yahoo.co.jp>
2020-05-27 18:10:19 +0900

    register fix

---
+++ data/schema.sql
@@ -0,0 +1,19 @@
+CREATE table user(
+     id int primary key,
+     username text not null,
+     password text not null,
+     teamname text not null
+);
+
+CREATE table pitcher_data(
+       data_id int primary key,
+       pitcher_name text not null,
+       day int not null,
+       pitch_type text not null,
+       pitch_speed real not null,
+       rotations int not null,
+       r_efficiency int not null,
+       v_change real not null,
+       h_change real not null,
+       id int not null
+);
